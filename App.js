8b86f70f051c02899e0f076245ff08e1eacf60cc
PYETRO FABRÍCIO PETERLINI <pyetro.peterlini@etec.sp.gov.br>
2025-08-06 10:12:22 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Tela1Screen from './src/screens/Tela1Screen';
+import Tela2Screen from './src/screens/Tela2Screen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Tela1'>
+        <Stack.Screen name="Tela1" component={Tela1Screen} />
+        <Stack.Screen name="Tela2" component={Tela2Screen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/Tela1Screen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import casual_dog from '../../assets/casual_dog.png';
+
+export default function Tela1({ navigation }) {
+    
+}
+
+<Image 
+    source={casual}
+/>
